06e0852bc8d452a1e487b51864edb450dfb5c7fb
Andrew Rea <andrew.s.rea@gmail.com>
2019-06-17 13:13:03 -0700

    New changes

--- liri.js
+++ liri.js
@@ -1,30 +1,44 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var nodeSpotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
 
+var moment = require("moment");
+moment().format();
+
+var axios = require("axios");
+
+var fs = require("fs");
+
 var command = process.argv[2];
-var query = process.argv[3];
+var value = process.argv[3];
 
-var artist = ""
-
-"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+if(command === "concert-this") {
+	concertThis(value);
+} else if(command === "spotify-this-song") {
+	spotifyThisSong(value);
+} else if(command === "movie-this") {
+	movieThis(value);
+} else if(command === "do-what-it-says") {
+    doIt(value);
+};
 
 // 1. node liri.js concert-this <artist/band name here>
 
-function concertThis(artist) {
-    axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+function concertThis(value) {
+    axios.get("https://rest.bandsintown.com/artists/" + value + "/events?app_id=codingbootcamp")
     .then(function(response) {    
         for (var i = 0; i < response.data.length; i++) {
 
-            var datetime = response.data[i].datetime; //Saves datetime response into a variable
-            var dateArr = datetime.split('T'); //Attempting to split the date and time in the response
+            var datetime = response.data[i].datetime;
+            var dateArr = datetime.split('T');
 
             var concertResults = 
                 "--------------------------------------------------------------------" +
                     "\nVenue Name: " + response.data[i].venue.name + 
                     "\nVenue Location: " + response.data[i].venue.city +
-                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY"); //dateArr[0] should be the date separated from the time
+                    "\nDate of the Event: " + moment(dateArr[0], "MM-DD-YYYY");
             console.log(concertResults);
         }
     })
@@ -87,24 +101,12 @@ function movieThis(value) {
 
 // 4. node liri.js do-what-it-says
 
-
-
-//Commands: concert-this; spotify-this-song; movie-this; do-what-it-says; will run functions when typed in Terminal
-//node liri.js concert-this <artist/band name here> will search the Bands In Town Events API 
-//("https://rest.bandsintown.com/artists/" + artist + "events?app_id=codingbootcamp") for an artist and render the following info about each event to the Terminal:
-//1) name of venue; 2) venue location; 3) date of the event (use moment to format this as "MM/DD/YYYY")
-
-//node liri.js spotify-this-song '<song name here>' will show the following info about the song in your Terminal:
-//1) artist; 2) song's name; 3) a preview link of the song from Spotify; 4) the album that the song is from;
-//(If no song is provided, then program will default to "The Sign" from Ace of Base.) [Read steps for Spotify API.]
-
-// App functionality due to user input
-if(command === "concert-this") {
-	concertThis();
-} else if(command === "spotify-this-song") {
-	spotifyThisSong(query);
-} else if(command === "movie-this") {
-	movieThis(query);
-} else if(command === "do-what-it-says") {
-	// App functionality from file read / loads fs npm package
-	var fs = require("fs");
+function doIt(value) {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+        spotifyThisSong(dataArr[0], dataArr[1]);
+    })
+}
